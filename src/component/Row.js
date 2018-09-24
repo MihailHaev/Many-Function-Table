575607dbc7e9098591af6f033ec4ba7c523298c3
MihailHavaev <khriokhriokh@icloud.com>
2018-09-24 22:59:44 +0300

    Row of Table

--- src/component/Row.js
+++ src/component/Row.js
@@ -1,8 +1,9 @@
 import React from 'react'
-
+// Row - строка таблицы, dataElement obj
 function Row(props) {
    const dataElement = props.dataElement
   return ( [
+    // Массив ячеек таблицы
       <td key={dataElement.id}>{dataElement.id}</td>,
       <td key={dataElement.firstName}>{dataElement.firstName}</td>,
       <td key={dataElement.lastName}>{dataElement.lastName}</td>,
